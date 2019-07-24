a537911155ae2de9bf22d2db631c45bd40c9065b
M0L1Y <canojuan9@gmail.com>
2019-07-24 10:50:34 -0500

    work

---
+++ js/main.js
@@ -0,0 +1,67 @@
+/*var nomb=prompt("ingrese el nombre del estudiante");
+console.log(nomb);
+var edad=prompt("ingrese su edad actual");
+console.log(edad);
+var gra=prompt("ingrese ultimo grado de escolarida cursado");
+console.log(gra);*/
+
+//PROMEDIO
+
+/*var mat=prompt("ingrese el nombre de la materia");
+console.log(mat);
+var not1=parseFloat(prompt("ingrese nota 1"));
+console.log(not1);
+var not2=parseFloat(prompt("ingrese nota 2"));
+console.log(not2);
+var not3=parseFloat(prompt("ingrese nota 3"));
+console.log(not3);
+var not4=parseFloat(prompt("ingrese nota 4"));
+console.log(not4);
+var prom=((not1+not2+not3+not4)/4);
+console.log(prom);*/
+
+//ejercicio 2
+/*var mat=prompt("ingrese el nombre de la materia");
+console.log(mat);
+var not1=parseFloat(prompt("ingrese nota 1"));
+console.log(not1);
+var not2=parseFloat(prompt("ingrese nota 2"));
+console.log(not2);
+var not3=parseFloat(prompt("ingrese nota 3"));
+console.log(not3);
+var not4=parseFloat(prompt("ingrese nota 4"));
+console.log(not4);
+var not5=parseFloat(prompt("ingrese nota 5"));
+console.log(not5);
+var not6=parseFloat(prompt("ingrese nota 6"));
+console.log(not6);
+var prom=((not1*0.125+not2*0.125+not3*0.125+not4*0.125)+(not5*0.25+not6*0.25));
+console.log(prom);*/
+
+
+//ejercicio 4
+/*var mat=prompt("ingrese el nombre de la materia");
+console.log(mat);
+var not1=parseFloat(prompt("ingrese nota 1"));
+console.log(not1);
+var not2=parseFloat(prompt("ingrese nota 2"));
+console.log(not2);
+var not3=parseFloat(prompt("ingrese nota 3"));
+console.log(not3);
+var not4=parseFloat(prompt("ingrese nota 4"));
+console.log(not4);
+var not5=parseFloat(prompt("ingrese nota 5"));
+console.log(not5);
+var prom=((not1*0.33+not2*0.33+not3*0.33)/3*0.5)+(not4*0.25+not5*0.25);
+console.log(prom);
+if(prom>=3){
+alert("gano");}
+else if(prom<3){
+alert("perdio perverso");
+}
+else if(prom==5){
+    alert("****FELICIDADES****")
+}
+else{
+
+}*/
